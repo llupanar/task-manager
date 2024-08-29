dda8f6a72abb38aeece12003b7c98ca75a1d1db1
Vera <shijion@mail.ru>
2024-08-29 09:28:13 +0300

    add header

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskListComponent } from './features/components/task-list/task-list.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TaskListComponent],
+  imports: [RouterOutlet, TaskListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
