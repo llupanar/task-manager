89f6a318597a0c0e6d29ba876b184d440048afdd
Vera <shijion@mail.ru>
2024-09-05 22:48:24 +0300

    ngrx integration

--- todo-list/src/app/app.config.ts
+++ todo-list/src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { taskReducer } from './core/storage/task.reducer';
+import { TaskService } from './features/service/task.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore({tasks: taskReducer}),TaskService]
 };
